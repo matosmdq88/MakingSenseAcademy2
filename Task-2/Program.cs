e81cec8b81bb305f17b30906a40251fc561258b4
matosmdq88 <matosmdq88@gmail.com>
2021-08-19 00:22:21 -0300

    [Task_2] Demo: Variables and Constants
    * Declarate different variables using var, and declarate a constant.
    * Variable console printing.

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -10,6 +10,20 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            var number = 9;
+            var firstName = "matias";
+            var character = 'F';
+            var floatNumber = 27.9f;
+            var itsOk = true;
+            const float Pi = 3.14f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(firstName);
+            Console.WriteLine(character);
+            Console.WriteLine(floatNumber);
+            Console.WriteLine(itsOk);
+            Console.WriteLine("Pi value its= "+Pi);
+            Console.WriteLine("{0} {1}", int.MinValue,int.MaxValue);
         }
     }
 }
