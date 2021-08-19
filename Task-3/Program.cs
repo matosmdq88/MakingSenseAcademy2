29869ee41d5e66569af484349900472e14da9af1
matosmdq88 <matosmdq88@gmail.com>
2021-08-19 01:14:54 -0300

    [Task_3] Demo: type conversion
    *Different type of conversions.
    
    *Exception handle with try and catch

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -10,6 +10,34 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            //Implicit type conversion
+            byte num1 = 15;
+            int num2 = num1;
+
+            Console.WriteLine(num2);
+
+            //Explicit type conversion
+            float num3 = 19.0f;
+            long num4 = (long)num3; //here we do casting from float to long type
+
+            Console.WriteLine(num4);
+
+            //Conversion between NON-compatible types
+
+            var numStr = "1526";
+            int num5 = Convert.ToInt32(numStr); //here we convert from string to int type
+
+            Console.WriteLine(num5);
+
+            try  //try catch block used to handle exeption
+            {
+                var numStr1 = "1526";
+                byte num6 = Convert.ToByte(numStr1); //here we convert from string to byte type
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("imposible to convert from string to byte because number is above 255");
+            }
         }
     }
 }
