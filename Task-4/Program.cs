051e67c6d3aa3166f999ad168469946bc7e90470
matosmdq88 <matosmdq88@gmail.com>
2021-08-19 23:47:19 -0300

    [Task_4] *demo: Operators (work)
    *Work with different operators

--- Task-4/Program.cs
+++ Task-4/Program.cs
@@ -10,6 +10,38 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
+            //declarate variables and use arithmetic operators
+            var a = 45;
+            var b = 0;
+            var c = b+3;
+            var d = (float)a / (float)c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            //postfix and prefix increment
+            a = 10;
+            b = a++;
+            Console.WriteLine("postfix");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = 10;
+            b = ++a;
+            Console.WriteLine("prefix");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            //comparison operators
+            a = 10;
+            b = 20;
+            c = 5;
+            Console.WriteLine("Comparison operators");
+            Console.WriteLine(a!=b);
+            Console.WriteLine(b > a && c==a/2);
+
         }
     }
 }
