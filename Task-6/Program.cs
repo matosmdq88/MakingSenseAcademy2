a26bb7c6bcea7f285630f5195083527e26520828
matosmdq88 <matosmdq88@gmail.com>
2021-08-23 01:26:29 -0300

    [Task_6]demo Arrays & Strings:
    *Practice with Arrays

--- Task-6/Program.cs
+++ Task-6/Program.cs
@@ -10,6 +10,17 @@ namespace Task_6
     {
         static void Main(string[] args)
         {
+            var stringArray = new string[5];
+            stringArray[0] = "blue";
+            stringArray[1] = "white";
+            stringArray[2] = "black";
+            stringArray[3] = "red";
+            stringArray[4] = "grey";
+
+            for (int a = 0; a < 5; a++)
+            {
+                Console.WriteLine("the colour is: "+stringArray[a]);
+            }
         }
     }
 }
