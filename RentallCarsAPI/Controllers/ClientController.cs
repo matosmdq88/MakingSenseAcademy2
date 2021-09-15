34afd3a1b385aec5f5512cc04abca1efa3e13def
matosmdq88 <matosmdq88@gmail.com>
2021-09-14 23:17:56 -0300

    [ClientController] Add Update & Delete method

--- RentallCarsAPI/Controllers/ClientController.cs
+++ RentallCarsAPI/Controllers/ClientController.cs
@@ -92,6 +92,96 @@ namespace RentallCarsAPI.Controllers
             }
             response.Succes = true;
             response.Message = "Found successfully";
+            response.Data = clients;
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult Update(ClientRequest model)
+        {
+            var response = new Response();
+
+            var clients = _clientHelper.GetAll();
+            if (clients == null)
+            {
+                response.Message = "File reading failed";
+                return BadRequest(response);
+            }
+            var isClientDni = _clientHelper.ValidateDni(model);
+            if (!isClientDni)
+            {
+                response.Message = "Invalid DNI to update";
+                return BadRequest(response);
+            }
+            
+            foreach (var client in clients)
+            {
+                if (client.Dni == model.Dni)
+                {
+                    client.Dni = model.Dni;
+                    client.FirstName = model.FirstName;
+                    client.LastName = model.LastName;
+                    client.Phone = model.Phone;
+                    client.Address = model.Address;
+                    client.City = model.City;
+                    client.Province = model.Province;
+                    client.PostalCode = model.PostalCode;
+                    client.LastModification = DateTime.Now;
+                    response.Data = client;
+                    break;
+                }
+            }
+
+            try
+            {
+                var writer = JsonConvert.SerializeObject(clients, Formatting.Indented);
+                System.IO.File.WriteAllText(_configuration.GetValue<string>("MySettings:_pathclients"), writer);
+                response.Succes = true;
+            }
+            catch (Exception)
+            {
+                response.Message = "Impossible to Update";
+                return BadRequest(response);
+            }
+            response.Message = "Successfully updated";
+            return Ok(response);
+        }
+
+        [HttpDelete("{Dni}")]
+        public IActionResult Delete(int Dni)
+        {
+            var response = new Response();
+
+            var clients = _clientHelper.GetAll();
+            if (clients == null)
+            {
+                response.Message = "File reading failed";
+                return BadRequest(response);
+            }
+
+            var clientToDelete = clients.FirstOrDefault(client => client.Dni == Dni);
+            if (clientToDelete == null)
+            {
+                response.Message = $"Car with id: {Dni} not found";
+                return NotFound(response);
+            }
+
+            response.Succes = clients.Remove(clientToDelete);
+            try
+            {
+                var writer = JsonConvert.SerializeObject(clients, Formatting.Indented);
+                System.IO.File.WriteAllText(_configuration.GetValue<string>("MySettings:_pathclients"), writer);
+            }
+
+            catch (Exception)
+            {
+                response.Succes = false;
+                response.Message = "Impossible to Delete";
+                return BadRequest(response);
+            }
+
+            response.Data = clientToDelete;
+            response.Message = "Delete successfully";
             return Ok(response);
         }
     }
